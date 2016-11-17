4991e08c5f944e9433991ae61a6ff8e7762ebc0b
amliomnie <mimiheadmeng@gmail.com>
2016-11-17 20:35:34 +0700

    create project w/order button

---
+++ BufTest/app/src/main/java/com/example/buftest/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.buftest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void gotoOrder(View v){
+
+        Intent gotoOrder = new Intent(this, OrderActivity.class);
+        startActivity(gotoOrder);
+
+    }
+
+
+}
