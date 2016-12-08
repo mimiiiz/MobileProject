1d91d6c73654cd39fbc15e695a092ee58f57ef16
amliomnie <mimiheadmeng@gmail.com>
2016-12-08 22:04:48 +0700

    gen code

---
+++ BufTest/app/src/main/java/com/example/buftest/model/Code.java
@@ -0,0 +1,37 @@
+package com.example.buftest.model;
+
+import java.util.Date;
+
+/**
+ * Created by User on 8/12/2559.
+ */
+
+public class Code {
+
+    private String code, TableNo;
+    private Date timeStamp;
+
+    public Date getTimestamp() {
+        return timeStamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timeStamp = timestamp;
+    }
+
+    public String getTableNo() {
+        return TableNo;
+    }
+
+    public void setTableNo(String tableNo) {
+        TableNo = tableNo;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
