b18bca3090d52e2e3b5b9df3d41b7871e570be59
amliomnie <mimiheadmeng@gmail.com>
2016-12-11 17:33:48 +0700

    delete menu

--- BufTest/app/src/main/java/com/example/buftest/MenuActivity.java
+++ BufTest/app/src/main/java/com/example/buftest/MenuActivity.java
@@ -1,10 +1,13 @@
 package com.example.buftest;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -31,36 +34,89 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+
+        lv_listMenu = (ListView) findViewById(R.id.lv_listMenu);
         mDatabase = FirebaseDatabase.getInstance().getReference();
         Query mQ = mDatabase.child("Menu");
         mQ.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                 menuLs = new ArrayList<Menu>();
-                menuNames = new ArrayList<String>();
-                lv_listMenu = (ListView) findViewById(R.id.lv_listMenu);
-                     for (DataSnapshot mSnap : dataSnapshot.getChildren()) {
-                         Menu menuObj = new Menu();
-                         menuObj = mSnap.getValue(Menu.class);
-                         menuLs.add(menuObj);
-                         menuNames.add(menuObj.getMenuName());
-                     }
-                     Log.d("Menu, onCreate", "onDataChange: Size of arrayList Menu " + menuLs.size());
+                                     @Override
+                                     public void onDataChange(DataSnapshot dataSnapshot) {
+                                         menuLs = new ArrayList<Menu>();
+                                         menuNames = new ArrayList<String>();
+                                         for (DataSnapshot mSnap : dataSnapshot.getChildren()) {
+                                             Menu menuObj = new Menu();
+                                             menuObj = mSnap.getValue(Menu.class);
+                                             menuLs.add(menuObj);
+                                             menuNames.add(menuObj.getMenuName());
+                                         }
+                                         Log.d("Menu, onCreate", "onDataChange: Size of arrayList Menu " + menuLs.size());
 
-                lv_listMenu.setAdapter(new ArrayAdapter<String>(MenuActivity.this, android.R.layout.simple_list_item_1, menuNames));
-            }
-            @Override
-            public void onCancelled (DatabaseError databaseError){
+                                         lv_listMenu.setAdapter(new ArrayAdapter<String>(MenuActivity.this, android.R.layout.simple_list_item_1, menuNames));
+                                     }
+
+                                     @Override
+                                     public void onCancelled(DatabaseError databaseError) {
 
                                      }
-        }
+                                 }
 
         );
 
+        lv_listMenu.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+                showDeleteMenuDialog(position);
+                return true;
+            }
+        });
+
     }
 
-    protected void gotoAddMenu(View v){
+    protected void gotoAddMenu(View v) {
         Intent gotoAddMenu = new Intent(this, AddMenuActivity.class);
         startActivity(gotoAddMenu);
+    }
+
+    public boolean showDeleteMenuDialog(final Integer position) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(MenuActivity.this);
+        alert.setTitle("Delete this menu?");
+        alert.setMessage("Do you want to delete this menu? \n It make this menu cannot order");
+
+        alert.setPositiveButton("Remove", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                deleteMenu(position);
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            //user click cancle
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alert.show();
+        return true;
+    }
+
+    protected void deleteMenu(final int position) {
+        mDatabase.child("Menu").orderByChild("menuName").equalTo(menuLs.get(position).getMenuName()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChildren()) {
+                    DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                    Log.d("firstChild", firstChild.getRef() + "");
+                    firstChild.getRef().removeValue();
+                    menuLs.remove(position);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
